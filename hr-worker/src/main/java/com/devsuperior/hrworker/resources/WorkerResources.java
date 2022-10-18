ba898eebfb2f7f5cb4b2eb2e15e9c38060dad59f
janioalbuquerque <aurelio.janio@gmail.com>
2022-10-18 15:32:38 -0300

    Zuul timeout

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResources.java
@@ -37,12 +37,12 @@ public class WorkerResources {
 	@GetMapping(value="/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable long id){
 
-//			try {
-//				Thread.sleep(3000L);
-//			} catch (InterruptedException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
+			try {
+				Thread.sleep(3000L);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		
 		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
